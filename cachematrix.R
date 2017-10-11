d171e9ae0369b5703f2f6bca99fc22a57b4c37dc
bestfriendsclub <r.beer92@gmail.com>
2017-10-11 13:50:38 +1300

    hei

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix() creates an object with information about a matrix and fucntions used to edit and inspect that matrix
+## cacheSolve() gets or sets then prints the inverse of makeCacheMatrix()'s matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv<-NULL
+  set<-function(y){
+    x<<-y
+    inv<<-NULL
+  }
+  get<-function() x 
+  setinv<-function(i){
+    inv<<-i
+  }
+  getinv<-function() inv
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  i<-x$getinv()
+  if(!is.null(i)){
+    message("getting chached inverse")
+    return(i)
+  }
+  m<-x$get()
+  i<-solve(m)
+  x$setinv(i)
+  i
 }
