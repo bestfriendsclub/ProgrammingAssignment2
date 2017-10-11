931bc86158c722a4dfbf47cae0c9f9d64827adf1
bestfriendsclub <r.beer92@gmail.com>
2017-10-11 15:36:59 +1300

    richard

--- cachematrix.R
+++ cachematrix.R
@@ -1,29 +1,42 @@
-## makeCacheMatrix() creates an object with information about a matrix and fucntions used to edit and inspect that matrix
-## cacheSolve() gets or sets then prints the inverse of makeCacheMatrix()'s matrix
+## cachematrix calculates and caches a matrix's inverse
 
+## makeCacheMatrix(x) returns a list of functions for inspecting and editing x (a matrix)
 makeCacheMatrix <- function(x = matrix()) {
+  ## inv is the cache for x's inverse
   inv<-NULL
+  ## set(y) changes x to y and clears inv
   set<-function(y){
     x<<-y
     inv<<-NULL
   }
+  ## get() returns x
   get<-function() x 
+  ## setinv(i) caches i to inv
   setinv<-function(i){
     inv<<-i
   }
+  ## getinv() returns inv
   getinv<-function() inv
+  ## makeCacheMatrix() returns a list of functions
   list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
-
-cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
-  i<-x$getinv()
+## cacheSolve(y, ...) returns the inverse of makeCacheMatrix(x)$get() (x)
+cacheSolve <- function(y, ...) {
+  ## i is y's (makeCacheMatrix(x)'s) cached inverse
+  i<-y$getinv()
+  ## if i is not null
+    ## cacheSolve(y, ...) returns i (a cached, current inverse matrix)
   if(!is.null(i)){
     message("getting chached inverse")
     return(i)
   }
-  m<-x$get()
+  ## if i is null
+    ## m is y's matrix parameter (x)
+  m<-y$get()
+    ## i is m's inverse
   i<-solve(m)
-  x$setinv(i)
+    ## y's matrix parameter is i
+  y$setinv(i)
+    ## cacheSolve(y, ...) returns i
   i
 }
